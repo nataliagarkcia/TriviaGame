a126365bb9cdc53c44c91a8e0ab728ada5a06ab9
nataliagarcia <nataliagarkciahotmail.com>
2016-07-17 18:29:04 -0400

    html css files upgrade

---
+++ assets/javascript/app.js
@@ -0,0 +1,28 @@
+//when the web page loads this function will start//
+$(document).ready(function(){
+
+			// Theme Button
+			$("#start").on("click", function(){
+        		audioElement.play();
+			});
+
+
+
+//question variables//
+ 
+var question1= 'Which one is the main character of nightmare before christmas?';
+var optionAnswers1=['Jack Skeleton','Mickey Mouse','Nemo','Bart Simpson'];
+
+var question2='Who is Zero?';
+var optionAnswers2=["I don't know",'a mouse ','a number',"Jacks's Dog"];
+
+var question3="Which one is Jack Skelligton's nickname?";
+var optionAnswer3=['the bubble','the pumpking king','mayor','oogie booggie']
+
+var question4="Who lives in Christmas Town";
+var optionAnswer4=['The Easter Bunny','Santa Claus','Thomas The Train','Dr.Flinkestein']
+
+var question5='What is Oogie Boogie filled with?';
+var optionAnswer5=['bugs','candies','love','trash'];
+
+
